0213a72abf45d6b6357a7611980d4bdf4f1b46b6
Pranay Sarepaka <gamebreakdownsbypranay@gmail.com>
2026-03-20 01:29:35 +0530

    fix: close startServer function

--- backend/immaculate-ipl-backend/server.js
+++ backend/immaculate-ipl-backend/server.js
@@ -401,10 +401,10 @@ async function startServer() {
 
   const PORT = process.env.PORT || 3000;
 
-  app.listen(PORT, () => {
+  app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
   });
 }
 
-// ✅ call it here (outside)
+// ✅ function closed properly
 startServer();
