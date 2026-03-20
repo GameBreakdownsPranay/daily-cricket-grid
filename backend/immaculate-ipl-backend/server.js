ffd7a4c4005deae35fb5e3d5aba44422fd6dc2bd
Pranay Sarepaka <gamebreakdownsbypranay@gmail.com>
2026-03-20 19:26:46 +0530

    CORS fix

--- backend/immaculate-ipl-backend/server.js
+++ backend/immaculate-ipl-backend/server.js
@@ -20,8 +20,18 @@ const app = express()
 
 app.set("trust proxy", 1)
 
-app.use(cors())
-app.use(express.json())
+const cors = require("cors");
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://daily-cricket-grid-c06m.vercel.app"
+  ],
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"]
+}));
+
+app.use(express.json());
 
 /* ---------------- RATE LIMIT ---------------- */
 
